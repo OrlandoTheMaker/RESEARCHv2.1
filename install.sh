608ecfc8eed58309d91e5bf7f92eae84cac9fee6
Orla-ndo <106558996+OrlandoTheMaker@users.noreply.github.com>
2023-06-27 17:28:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update && apt upgrade -y
 pkg install python3 -y
 pkg install git
-git clone https://github.com/OrlandoTheMaker/RESEARCHv1.1.git
+pkg install fish
 pip3 install -r requirements.txt
 python research.py
