31dd195004e70deaa9a2af60d1b4b79cb68be66a
Orla-ndo <106558996+OrlandoTheMaker@users.noreply.github.com>
2023-07-01 09:15:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 apt update && apt upgrade -y
 pkg install python3 -y
 pkg install git
@@ -8,4 +6,9 @@ pkg install fish
 pkg install pip
 pip3 install -r requirements.txt
 pkg install nmap
+pip install colorama
+pip install pyfiglet
+pip install bs4
+pip install requests
+pip install python-nmap
 python research.py
